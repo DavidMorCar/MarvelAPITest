43424f0a3fc2f3d8a83d552f062bca1c8b5268d2
davidamoreno <95384068+davidamoreno@users.noreply.github.com>
2022-04-08 02:11:23 -0700

    Readme update

--- app/src/main/java/com/davidmoreno/marvelapitest/common/Constants.kt
+++ app/src/main/java/com/davidmoreno/marvelapitest/common/Constants.kt
@@ -5,9 +5,9 @@ const val DELAY: Long = 3000
 const val LONG_DELAY: Long = 10000
 const val NO_DESCRIPTION_MESSAGE = "No description available..."
 
-const val PUBLIC_CREDENTIALS = "6df33f27f951b1081ac455a681db35eb"
-const val HASH = "df86d062899f38736a0876569658dda2"
-const val TS = "1234"
+const val PUBLIC_CREDENTIALS = ""
+const val HASH = ""
+const val TS = ""
 const val ORDER = "title"
 
 const val BASE_URL = "https://gateway.marvel.com:443/v1/public/"
